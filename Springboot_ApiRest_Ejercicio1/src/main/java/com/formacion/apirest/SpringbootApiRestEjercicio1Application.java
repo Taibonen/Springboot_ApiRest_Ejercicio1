c21280bc94d739100dbb352e570a3ce0d2113f26
Jon Alonso <Jon.Alonso@inetum.com>
2022-11-16 12:11:40 +0100

    Inicial

---
+++ Springboot_ApiRest_Ejercicio1/src/main/java/com/formacion/apirest/SpringbootApiRestEjercicio1Application.java
@@ -0,0 +1,13 @@
+package com.formacion.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiRestEjercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiRestEjercicio1Application.class, args);
+	}
+
+}
